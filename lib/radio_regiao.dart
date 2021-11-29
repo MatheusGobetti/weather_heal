ec2f012f50d196fe3976422e26a4e25fcd94f636
Matheus Gobetti <matheusgobetti12@gmail.com>
2021-11-29 14:49:19 -0300

    Radio regiao template

--- lib/radio_regiao.dart
+++ lib/radio_regiao.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
 
-enum Regiao { atual, outro }
-
 class RadioRegiao extends StatefulWidget {
   const RadioRegiao({Key? key}) : super(key: key);
 
@@ -10,58 +8,10 @@ class RadioRegiao extends StatefulWidget {
 }
 
 class _RadioRegiaoState extends State<RadioRegiao> {
-  Regiao? _regiao = Regiao.atual;
-
   @override
   Widget build(BuildContext context) {
     return Column(
-      children: <Widget>[
-        ListTile(
-          title: RichText(
-            text: TextSpan(children: [
-              TextSpan(
-                  text: 'Ribeirão Preto, ',
-                  style: TextStyle(
-                      color: Colors.black,
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold)),
-              TextSpan(
-                  text: 'Brasil',
-                  style: TextStyle(color: Colors.black, fontSize: 20)),
-            ]),
-          ),
-          leading: Radio<Regiao>(
-            value: Regiao.atual,
-            groupValue: _regiao,
-            onChanged: (Regiao? value) {
-              setState(() {
-                _regiao = value;
-              });
-            },
-          ),
-        ),
-        ListTile(
-          title: RichText(
-            text: TextSpan(children: [
-              TextSpan(
-                  text: 'Outro',
-                  style: TextStyle(
-                    color: Colors.black,
-                    fontSize: 20,
-                  )),
-            ]),
-          ),
-          leading: Radio<Regiao>(
-            value: Regiao.outro,
-            groupValue: _regiao,
-            onChanged: (Regiao? value) {
-              setState(() {
-                _regiao = value;
-              });
-            },
-          ),
-        ),
-      ],
+      children: <Widget>[],
     );
   }
 }
